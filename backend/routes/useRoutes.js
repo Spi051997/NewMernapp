ab0e58dbb2c8491037abd2913c6acfdf8111b449
Spi051997 <dcodeacedemy97@gmail.com>
2022-07-09 15:47:06 +0530

    Search api

--- backend/routes/useRoutes.js
+++ backend/routes/useRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const  router = express.Router();
-const {registerUser,authUser}=require('../controller/userControllers')
+const {registerUser,authUser,allUsers}=require('../controller/userControllers')
+const { protect } = require("../middleware/authMiddleware");
 
 
-// router.route("/").get(protect, allUsers);
-// router.route("/").post(registerUser);
 router.post("/", registerUser);
 router.post("/login", authUser);
-
+router.route("/").get(protect, allUsers);   
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const dotenv=require('dotenv')
 var colors = require('colors');
 const userRoutes=require('./routes/useRoutes');
+const { notFound, errorHandler } = require("./middleware/errorhandlere");
 const dbConnection=require('./config/mongoose');
 dbConnection;
 const app=express();
@@ -9,6 +10,10 @@ dotenv.config();
 
 app.use(express.json())
 app.use('/api/user',userRoutes) // to accept the json data
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT=process.env.PORT;
 
